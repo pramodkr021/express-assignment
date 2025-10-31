57c3d65d5103e184bea00f17f4f7f2dd2d1d46b7
Pramod Kumar <pramod021kr@gmail.com>
2025-10-31 11:51:26 +0530

    restrict cors origin to frontend

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const port = 8000
 dbConnect();
 
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
 app.use("/tasks", taskRouter)
-// app.use("/users", userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
